3202e01f38d694e4f921091274b923e9707a550e
KurosakiRei <KurosakiRei@users.noreply.github.com>
2021-11-19 18:53:14 -0500

    update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
             "link",
             {
                 "rel": "icon",
-                "href": "/home/favicon.ico"
+                "href": "/favicon.ico"
             }
         ],
         [
